6f53016fd6d4697a6cb14248de15cdad8948328a
CauaSantos04 <cauasantos0411@gmail.com>
2024-11-24 11:25:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Quimica',
+    'O que é meia vida?',
+    'Meia vida é o processo de um isotopo ao perder a sua massa durante um periodo de tempo'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é a ONU?',
+    'Organização das Nações Unidas'
+)
+
+criaCartao(
+    'Cultura POP',
+    'O que é a comic Con?',
+    'Evento destinado ao encontro de pessoas que gostam  se interessam por quadrinhos, filmes e etc'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Menino em Inglês?',
+    'Menino em ingles é Boy'
+)
